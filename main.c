4249ba43cf8ad0bab69203f5469b1989c947060d
mingang.he <dustgle@gmail.com>
2018-05-13 23:25:05 +0800

    Update

--- main.c
+++ main.c
@@ -35,6 +35,8 @@ long __get_reg(pid_t child, int off) {
 char *LOCAL_ADDR = "127.0.0.1";
 uint16_t LOCAL_PORT = 2080;
 bool is_enter = false;
+struct sockaddr_in proxy_sa;
+int fifo_fd;
 
 void getdata(pid_t child, long addr, char *dst, int len)
 {
@@ -117,12 +119,57 @@ int do_child(const char *file, char *argv[])
 
 void socket_pre_handle(pid_t pid)
 {
-  // TODO
+  struct user_regs_struct regs;
+  struct socket_info *si = malloc(sizeof(*si));
+  ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+  assert(errno == 0);
+  si->domain = regs.rdi;
+  si->type = regs.rsi;
+  ptrace(PTRACE_SYSCALL, pid, 0, 0);
+  assert(errno == 0);
+  ptrace(PTRACE_GETREGS, pid, 0, &regs);
+  assert(errno == 0);
+  si->fd = (int)regs.rax;
+  si->is_connected = false;
+  add_socket_info(si);
 }
 
 void connect_pre_handle(pid_t pid)
 {
   // TODO
+  struct user_regs_struct regs;
+  ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+  assert(errno == 0);
+  int socket_fd = regs.rdi;
+  struct socket_info *so_info = find_socket_info(socket_fd);
+  if (so_info == NULL) {
+    fprintf(stderr, "find_socket_info(%d) return NULL\n", socket_fd);
+    exit(-1);
+  }
+  if (so_info->type != SOCK_STREAM || so_info->domain != AF_INET) {
+    ptrace(PTRACE_SYSCALL, pid, 0, 0);
+    return;
+  }
+  long addr = get_reg(pid, rsi);
+  struct sockaddr_in tmp_sa;
+  getdata(pid, addr, (char *)&tmp_sa, sizeof(tmp_sa));
+  unsigned int ip_int = SOCKADDR(tmp_sa);
+  unsigned short ip_port = SOCKPORT(tmp_sa);
+  struct in_addr tmp_ip_addr;
+  tmp_ip_addr.s_addr = ip_int;
+  putdata(pid, addr, (char *)&proxy_sa, sizeof(proxy_sa));
+  assert(errno == 0);
+  ptrace(PTRACE_SYSCALL, pid, 0, 0);
+
+  char buf[1024] = {0};
+  strcpy(buf, inet_ntoa(tmp_ip_addr));
+  strcat(&buf[strlen(buf)], ":");
+  sprintf(&buf[strlen(buf)], "%d\n", ntohs(ip_port));
+  fprintf(stderr, "%d: buf: %s\n", __LINE__, buf);
+  ssize_t wlen;
+  wlen = write(fifo_fd, buf, strlen(buf));
+  fprintf(stderr, "%s: %d: write: %d\n", __FILE__, __LINE__, wlen);
+  so_info->is_connected = true;
 }
 
 int wait_syscall_enter_stop()
@@ -140,12 +187,16 @@ int wait_syscall_enter_stop()
   syscall_num = get_reg(pid, orig_rax);
   fprintf(stderr, "%d: %s: syscall_num: %d\n",
       __LINE__, __func__, syscall_num);
-  if (syscall_num == SYS_socket) {
+  switch (syscall_num) {
+  case SYS_socket:
     socket_pre_handle(pid);
-  } else if (syscall_num == SYS_connect) {
+    break;
+  case SYS_connect:
     connect_pre_handle(pid);
+    break;
+  default:
+    ptrace(PTRACE_SYSCALL, pid, 0, 0);
   }
-  ptrace(PTRACE_SYSCALL, pid, 0, 0);
   return 0;
 }
 
@@ -199,7 +250,6 @@ int main(int argc, char **argv)
   int status;
   struct user *user_space = (struct user *) 0;
   struct user_regs_struct regs;
-  int fifo_fd;
   long tmp_addr;
 
   if (argc < 2) {
@@ -213,8 +263,6 @@ int main(int argc, char **argv)
     exit(errno);
   }
 
-  struct sockaddr_in proxy_sa;
-  struct sockaddr_in tmp_sa;
   socklen_t proxy_addrlen = sizeof(proxy_sa);
   proxy_sa.sin_family = AF_INET;
   proxy_sa.sin_port = htons(LOCAL_PORT);
