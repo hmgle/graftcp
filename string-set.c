81b3870c0f839df088d0050e326edfc4f914f7ee
mingang.he <dustgle@gmail.com>
2018-07-31 12:34:08 +0800

    Eliminate compilation warning

--- string-set.c
+++ string-set.c
@@ -24,7 +24,7 @@ static unsigned long str_hash(const char *str)
 	unsigned long hash = 5381;
 	int c;
 
-	while (c = *str++)
+	while ((c = *str++) != '\0')
 		hash = ((hash << 5) + hash) + c;	/* hash * 33 + c */
 
 	return hash;
