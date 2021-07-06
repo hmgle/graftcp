6bc34674ea7c4f3235137b3a03dcfe1e79c69083
mingang.he <dustgle@gmail.com>
2021-07-06 12:43:15 +0000

    Update args

--- local/cmd/mgraftcp/main.go
+++ local/cmd/mgraftcp/main.go
@@ -15,7 +15,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 	"syscall"
 	"unsafe"
 
@@ -42,29 +41,6 @@ func clientMain(args []string) int {
 
 	returnValue := C.client_main(argc, (**C.char)(unsafe.Pointer(argv)))
 	return int(returnValue)
-}
-
-func constructArgs(args []string) ([]string, []string) {
-	server := make([]string, 0, len(args))
-	client := make([]string, 0, len(args))
-
-	isCommand := false
-	for _, arg := range args {
-		if isCommand {
-			client = append(client, arg)
-		} else {
-			if strings.HasPrefix(arg, "--server") {
-				server = append(server, strings.TrimPrefix(arg, "--server"))
-			} else if strings.HasPrefix(arg, "--client") {
-				client = append(client, strings.TrimPrefix(arg, "--client"))
-			} else {
-				isCommand = true
-				client = append(client, arg)
-			}
-		}
-	}
-
-	return server, client
 }
 
 var (
@@ -109,14 +85,8 @@ func main() {
 	retCode := 0
 	defer func() { os.Exit(retCode) }()
 
-	// todo: we need special handle on args like '--help' which trigger os.Exit
-	// todo: randomly set and detect port number if no one specified
-	serverArgs, clientArgs := constructArgs(os.Args[1:])
-
-	clientArgs = append(os.Args[:1], clientArgs...)
-
-	// TODO: config args
 	l := local.NewLocal(":0", socks5Addr, socks5User, socks5Pwd, httpProxyAddr)
+	l.SetSelectMode(selectProxyMode)
 
 	tmpDir, err := ioutil.TempDir("/tmp", "mgraftcp")
 	if err != nil {
@@ -142,9 +112,9 @@ func main() {
 	_, faddr := l.GetFAddr()
 
 	var fixArgs []string
-	fixArgs = append(fixArgs, clientArgs[0])
+	fixArgs = append(fixArgs, os.Args[0])
 	fixArgs = append(fixArgs, "-p", strconv.Itoa(faddr.Port), "-f", pipePath)
-	fixArgs = append(fixArgs, clientArgs[1:]...)
-	log.Printf("serverArgs: %+v, fixArgs: %+v\n", serverArgs, fixArgs)
+	fixArgs = append(fixArgs, args[0:]...)
+	log.Printf("fixArgs: %+v\n", fixArgs)
 	retCode = clientMain(fixArgs)
 }
